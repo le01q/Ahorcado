bbb4487794f4b62ec23d50d95e99ab4bae88d62d
leoarayav <laraya1818@gmail.com>
2022-06-04 16:35:17 -0400

    Utils: Constantes del archivo principal agregadas.

---
+++ source/utils/constantes.py
@@ -0,0 +1,35 @@
+from colorama import Fore
+
+"""
+    Almacenamiento de constantes, detalles y más en este archivo.
+
+    Author:
+        Leo Araya | aka: le01q [github.com/le01q] [github.com/leoarayav]
+
+    Copyright:
+        Ahorcao (C) 2022 - All rights reserved 
+"""
+
+LOGO = """
+             _    _    ____    _____     _____              _____     ____  
+     /\     | |  | |  / __ \  |  __ \   / ____|     /\     |  __ \   / __ \ 
+    /  \    | |__| | | |  | | | |__) | | |         /  \    | |  | | | |  | |
+   / /\ \   |  __  | | |  | | |  _  /  | |        / /\ \   | |  | | | |  | |
+  / ____ \  | |  | | | |__| | | | \ \  | |____   / ____ \  | |__| | | |__| |
+ /_/    \_\ |_|  |_|  \____/  |_|  \_\  \_____| /_/    \_\ |_____/   \____/
+"""
+
+AUTHOR = "Leo Araya [github.com/le01q | github.com/leoarayav]"
+VERSION = "1.0.0"
+DATE = "04-06-2022"
+
+# Banner oficial para decoración.
+
+BANNER = f"""
+        {Fore.GREEN}{LOGO}{Fore.RESET}
+        - - - - - - - - - - - - - - - - - - - - - - - -
+        * Desarrollado por: {Fore.MAGENTA}{AUTHOR}{Fore.RESET}
+        * Version actual: {Fore.MAGENTA}{VERSION}{Fore.RESET}
+        * Fecha de lanzamiento: {Fore.MAGENTA}{DATE}{Fore.RESET}
+        - - - - - - - - - - - - - - - - - - - - - - - -
+"""
